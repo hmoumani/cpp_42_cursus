32040c2de70e20669c92e063696259972f4c59d1
Houssam-eddine Moumanis <hmoumani@e1r13p12.1337.ma>
2021-02-02 19:57:13 +0100

    fixing directories

---
+++ ex01/Contact.class.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.class.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmoumani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/02 15:38:55 by hmoumani          #+#    #+#             */
+/*   Updated: 2021/02/02 15:38:59 by hmoumani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_CLASS_H
+# define CONTACT_CLASS_H
+# include <iostream>
+class Contact
+{
+public:
+	std::string first_name;
+	std::string last_name;
+	std::string nick_name;
+	std::string login;
+	std::string postal_address;
+	std::string email_address;
+	std::string phone_number;
+	std::string birthday_date;
+	std::string favorite_meal;
+	std::string underwear_color;
+	std::string darkest_secret;
+	Contact(void);
+	void add(void);
+	void search(void);
+	~Contact(void);
+};
+
+#endif
