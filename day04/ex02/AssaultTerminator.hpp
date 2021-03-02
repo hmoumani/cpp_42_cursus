f44331035bc40c0f924736caec367e196044dcfc
Houssam-eddine Moumanis <hmoumani@e1r13p12.1337.ma>
2021-03-02 15:18:20 +0100

    adding header

--- day04/ex02/AssaultTerminator.hpp
+++ day04/ex02/AssaultTerminator.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AssaultTerminator.hpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmoumani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 15:16:28 by hmoumani          #+#    #+#             */
+/*   Updated: 2021/03/02 15:16:30 by hmoumani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ASSAULTTERMINATOR_HPP
 # define ASSAULTTERMINATOR_HPP
 # include "ISpaceMarine.hpp"
