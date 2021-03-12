3b20e9f4d21c5a6a2ccd4ad7d582581ce88a5f79
Houssam-eddine Moumanis <hmoumani@e1r13p12.1337.ma>
2021-03-12 16:46:43 +0100

    finishing day7/(ex00 && ex01)

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -11,6 +11,25 @@
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+class Awesome {
+
+public:
+
+Awesome( int n ) : _n( n ) {}
+
+bool operator==( Awesome const & rhs ) { return (this->_n == rhs._n); }
+bool operator!=( Awesome const & rhs ) { return (this->_n != rhs._n); }
+bool operator>( Awesome const & rhs ) { return (this->_n > rhs._n); }
+bool operator<( Awesome const & rhs ) { return (this->_n < rhs._n); }
+bool operator>=( Awesome const & rhs ) { return (this->_n >= rhs._n); }
+bool operator<=( Awesome const & rhs ) { return (this->_n <= rhs._n); }
+int  getN() const { return this->_n; }
+
+private:
+
+int _n;
+};
+std::ostream	&operator<<(std::ostream & out_stream, Awesome const & src){ return out_stream << src.getN(); };
 
 int main()
 {
@@ -48,6 +67,17 @@ int main()
         std::cout << "c = " << c << ", d = " << d << std::endl;
         std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
         std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    }
+    std::cout << "********************************" << std::endl;
+    {
+        Awesome a(1000);
+        Awesome b(2000);
+        std::cout << "a: " << a << std::endl;
+        ::swap( a, b );
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+        std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
     }
     return 0;
--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -28,9 +28,7 @@ T   &max(T & p1, T & p2)
 template <typename T>
 void    swap(T & p1, T & p2)
 {
-    T temp;
-
-    temp = p1;
+    T temp = p1;
     p1 = p2;
     p2 = temp;
 }
