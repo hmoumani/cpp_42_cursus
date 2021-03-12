55173c677edb2f3b7a49c7d64461a8f84cb03a99
Houssam-eddine Moumanis <hmoumani@e1r13p12.1337.ma>
2021-03-12 17:59:29 +0100

    adding day07/ex02

---
+++ day07/ex02/Array.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmoumani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/12 17:05:58 by hmoumani          #+#    #+#             */
+/*   Updated: 2021/03/12 17:06:00 by hmoumani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+# include <iostream>
+# include <exception>
+
+template <typename T>
+class Array
+{
+    private:
+        size_t _size;
+        T   *_list;
+    public:
+        Array<T>() : _size(0), _list(NULL)
+        {
+            _list = new T[_size]();
+        };
+        Array<T>(unsigned int n) : _size(n)
+        {
+            this->_list = new T[n];
+        };
+        Array<T>(const Array<T> &src) : _list(NULL)
+        {
+            *this = src;
+        };
+        ~Array<T>()
+        {
+            if (_list)
+                delete[] _list;
+        };
+        T &operator[](size_t i) const
+        {
+            if (i < 0 || i >= _size)
+                throw std::exception();
+            return _list[i];
+        }
+        Array<T>   &operator=(Array const & src)
+        {
+            if (_list)
+                delete[] _list;
+            this->_size = src._size;
+            _list = new T[_size];
+            for (size_t i = 0; i < _size; i++)
+            {
+                _list[i] = src._list[i];
+            }
+            return *this;
+        };
+        size_t     size(void) const { return this->_size; }
+};
+
+#endif
